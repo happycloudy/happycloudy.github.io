6794e590ca8960b1bd94198d632c90ddf270e640
Arion <arion.mikolae71@mail.ru>
2018-09-07 17:52:20 +0300

    2sites in 1 folder

---
+++ Brown_Theme/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.dropdown-trigger').dropdown();
+    $('.discovermore').on('click', function(){
+        $('.block2').jump();
+    });
+    
+
+    
+});
--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.dropdown-trigger').dropdown();
-    $('.discovermore').on('click', function(){
-        $('.block2').jump();
-    });
-    
-
-    
-});
