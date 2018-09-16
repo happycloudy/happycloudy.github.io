57785c3f4fdab69d255e29fbf663c995568a0991
Arion Mikolae <arion.mikolae71@mail.ru>
2018-09-16 23:14:44 +0300

    11

--- Brown_Theme/js/main.js
+++ Brown_Theme/js/main.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
         $('.block2').jump();
     });
     
-
+    $('.block2').on('mouseenter' , function(){
+        $('.block21').css('transform', 'translate(0)');
+    });
     
 });
